9117113cef0e07801fbf8a5e9ca0dca7460b6a09
Jöran Malek <alivedevil@outlook.com>
2014-12-22 18:26:05 +0100

    Added Player

---
+++ TextAdventure/Scenes/Components/Entities/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventure.Scenes.Components.Entities
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public sealed class Player : Entity
+	{
+		private const int baseHealth = 100;
+		private const int baseDamage = 10;
+
+		public Player(string name)
+			: base(name, baseDamage, baseHealth, null, null)
+		{
+		}
+	}
+}
